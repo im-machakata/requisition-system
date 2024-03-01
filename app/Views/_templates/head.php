d0eb7211985cbb35c4875ce4aa6f50b98b0f2d45
Isaac Machakata <isaac@caasi.co.zw>
2024-03-01 18:10:14 +0200

    refactor: connect to jquery

--- app/Views/_templates/head.php
+++ app/Views/_templates/head.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/static/css/style.css?revalidate=<?= time() ?>">
+    <script src="/static/js/jquery.3.7.1.min.js" defer async></script>
     <title><?= $this->data['title'] ?></title>
 </head>
 
