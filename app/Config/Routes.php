2d51477b0c2c38c2f0f2cd218d639a1571d7fd8e
Isaac Machakata <isaac@caasi.co.zw>
2024-03-01 18:02:45 +0200

    refactor: register routes for authentication

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Controllers\Auth;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+$routes->get('/auth/login', [Auth::class, 'login']);
+$routes->post('/auth/login', [Auth::class, 'createSession']);
