8ac97921a8084d43262be12ea38b71b3628a949d
Isaac Machakata <isaac@caasi.co.zw>
2024-03-05 09:55:10 +0200

    refactor: add accounts / auth table

---
+++ app/Database/Migrations/2024-03-04-190103_CreateAccountsTable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'ID' => [
+                'type' => 'INT',
+                'null' => true,
+                'unique' => true,
+                'constraint' => 8,
+                'auto_increment' => true
+            ],
+            'Username' => [
+                'type' => 'VARCHAR',
+                'constraint' => '20',
+                'unique' => true
+            ],
+            'Password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+        ]);
+        $this->forge->addPrimaryKey('ID');
+        $this->forge->createTable('accounts', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('accounts', true);
+    }
+}
