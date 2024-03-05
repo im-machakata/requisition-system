1bc4e5f3f87ab9750dba2a88e034dd11a1049137
Isaac Machakata <isaac@caasi.co.zw>
2024-03-05 09:55:43 +0200

    refactor: add employees table

---
+++ app/Database/Migrations/2024-03-04-191158_CreateEmployeeTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateEmployeeTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'ID' => [
+                'type' => 'INT',
+                'constraint' => 8,
+                'unique' => true,
+                'null' => true,
+                'auto_increment' => true
+            ],
+            'AccountID'=>[
+                'type' => 'INT',
+                'constraint' => 8,
+            ],
+            'Name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '20',
+            ],
+            'Surname' => [
+                'type' => 'VARCHAR',
+                'constraint' => '20',
+            ],
+            'Phone' => [
+                'type' => 'VARCHAR',
+                'constraint' => '15',
+            ],
+            'Email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '18',
+            ],
+            'DepartmentID' => [
+                'type' => 'INT',
+                'constraint' => '20',
+            ]
+        ]);
+        $this->forge->addPrimaryKey('ID');
+        $this->forge->addForeignKey('AccountID', 'accounts', 'ID');
+        $this->forge->addForeignKey('DepartmentID', 'departments', 'ID');
+        $this->forge->createTable('employees', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('employees', true);
+    }
+}
