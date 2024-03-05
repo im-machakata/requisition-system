d52180912c7b1b05a5aab8f705b26a46eaee7c53
Isaac Machakata <isaac@caasi.co.zw>
2024-03-05 11:24:44 +0200

    refactor: validate login credentials

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Entities\Employee;
+use App\Models\Account;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Auth extends BaseController
@@ -16,21 +17,36 @@ class Auth extends BaseController
     {
         $employee = new Employee($this->request->getPost());
         $sessionRules = [
-            'username' =>'required',
+            'username' => 'required',
             'password' => 'required',
         ];
 
         // stop execution if the validation fails
-        if(!$this->validate($sessionRules)){
+        if (!$this->validate($sessionRules)) {
             return view('auth/login', [
                 'error' => $this->validator->getErrors()
             ]);
         }
 
-        // todo: check if the username and password are correct
+        $accounts = model(Account::class);
+        $foundEmployee = $accounts->where('Username', $employee->username)->first();
+
+        if (!$foundEmployee) {
+            return view('auth/login', [
+                'error' => 'Invalid username or password'
+            ]);
+        }
+
+        // password has to be hashed before comparing, but disabled for plain passwords to work
+        // if (!password_verify($employee->password, $foundEmployee->password)) {
+        if ($employee->password !== $foundEmployee->Password) {
+            return view('auth/login', [
+                'error' => 'Invalid username or password'
+            ]);
+        }
 
         // save the employee data to the session
-        session()->set('user', $employee);
+        session()->set('user', $foundEmployee);
 
         // redirect to the home page
         return $this->response->redirect('/');
