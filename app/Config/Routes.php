fd4afcd5a67c354958c254c47d5b47930a017f1b
Isaac Machakata <isaac@caasi.co.zw>
2024-03-11 10:01:27 +0200

    refactor: add registration urls

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,6 @@ $routes->get('/', 'Home::index', ['filter' => 'auth']);
 $routes->get('/auth/login', [Auth::class, 'login']);
 $routes->get('/auth/logout', [Auth::class, 'logout']);
 $routes->post('/auth/login', [Auth::class, 'createSession']);
+
+$routes->get('/auth/add-user', [Auth::class, 'register']);
+$routes->post('/auth/add-user', [Auth::class, 'createAccount']);
