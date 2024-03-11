0b0e49c7a55bbb1da48e7c079bf1774db0a81bbd
Isaac Machakata <isaac@caasi.co.zw>
2024-03-11 23:28:01 +0200

    refactor: add system header component

---
+++ app/Views/_templates/header.php
@@ -0,0 +1,5 @@
+<div class="login-header text-center col-12">
+    <img src="/static/images/core5.png" alt="System Logo" class="img-flud rounded my-4" height="80">
+    <h1 class="fw-bold h5">Municipality of Rutenga</h1>
+    <p class="mb-4 text-muted h5">Online Requisition System</p>
+</div>
