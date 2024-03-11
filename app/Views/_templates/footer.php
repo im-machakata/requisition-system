077ee08aa8d2cc959734abaa971472ba29dbe45a
Isaac Machakata <isaac@caasi.co.zw>
2024-03-11 09:09:44 +0200

    refactor: use bootstrap

--- app/Views/_templates/footer.php
+++ app/Views/_templates/footer.php
@@ -1,4 +1,5 @@
 </body>
+<script src="/static/js/bootstrap.min.js?version=5.3.3" defer></script>
 <script src="/static/js/app.js?revalidate=<?= time() ?>" defer></script>
 
 </html>
--- app/Views/_templates/head.php
+++ app/Views/_templates/head.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/static/css/bootstrap.min.css?version=5.3.3">
     <link rel="stylesheet" href="/static/css/style.css?revalidate=<?= time() ?>">
     <script src="/static/js/jquery.3.7.1.min.js" defer async></script>
     <title><?= $this->data['title'] ?></title>
