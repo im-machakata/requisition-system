035c857dc17fb3f2559f84f6f29a827f3fae66ac
Isaac Machakata <isaac@caasi.co.zw>
2024-03-12 22:54:02 +0200

    refactor: make image link

--- app/Views/_templates/header.php
+++ app/Views/_templates/header.php
@@ -1,5 +1,7 @@
-<div class="login-header text-center col-12">
-    <img src="/static/images/core5.png" alt="System Logo" class="img-flud rounded my-4" height="80">
+<div class="text-center col-12">
+    <a href="/">
+        <img src="/static/images/core5.png" alt="System Logo" class="img-flud rounded my-4" height="80">
+    </a>
     <h1 class="fw-bold h5">Municipality of Rutenga</h1>
     <p class="mb-4 text-muted h5">Online Requisition System</p>
 </div>
