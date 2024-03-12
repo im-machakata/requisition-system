dfe5f90b5bb4919d204a5c03982b4f38593360f4
Isaac Machakata <isaac@caasi.co.zw>
2024-03-12 13:44:38 +0200

    refactor: add favicon

--- app/Views/_templates/head.php
+++ app/Views/_templates/head.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/static/css/bootstrap.min.css?version=5.3.3">
     <link rel="stylesheet" href="/static/css/style.css?revalidate=<?= time() ?>">
+    <link rel="shortcut icon" href="/static/images/core5.png" type="image/png">
     <script src="/static/js/jquery.3.7.1.min.js" defer async></script>
     <title><?= $this->data['title'] ?></title>
 </head>
