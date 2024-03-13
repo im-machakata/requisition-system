62f757aa4b53b79af2cd687a5d65c1effb0c09ef
Isaac Machakata <isaac@caasi.co.zw>
2024-03-13 11:34:22 +0200

    feat: show old username on error

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,7 +1,8 @@
 <?php
+extract($this->data);
 $this->setVar('title', 'Login');
 echo $this->include('_templates/head'); ?>
-<main class="d-flex flex-column justify-content-center align-items-center bg-primary" style="height: 100vh">
+<main class="d-flex flex-column justify-content-center align-items-center bg-primary" style="min-height: 100vh">
     <div class="row justify-content-center shadow rounded-4 p-3 bg-white mx-2" style="max-width: 400px;">
         <?php echo $this->include('_templates/header'); ?>
         <?php if (isset($this->data['error'])) :
@@ -10,7 +11,7 @@ echo $this->include('_templates/head'); ?>
         <form action="/auth/login" method="post" class="d-block col-12">
             <div class="mb-4">
                 <label for="username" class="form-label">Username</label>
-                <input type="text" name="username" id="username" class="form-control" placeholder="Username" autocomplete="off" required>
+                <input type="text" name="username" id="username" class="form-control" value="<?= $old('username') ?>" placeholder="Username" autocomplete="off" required>
             </div>
             <div class="mb-4">
                 <label for="password" class="form-label">Password</label>
