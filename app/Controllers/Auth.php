60f73c567238b6366d2e2314fe859d4fbdc247bf
Isaac Machakata <isaac@caasi.co.zw>
2024-03-13 11:54:13 +0200

    refactor: hash passwords refactor: add default old fields

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -3,25 +3,44 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Entities\Account as EntitiesAccount;
 use App\Entities\Employee;
 use App\Models\Account;
 use App\Models\Department;
 use App\Models\Employee as ModelsEmployee;
+use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
 
 class Auth extends BaseController
 {
+    /**
+     * Stores the default auth view functions such as old
+     *
+     * @var array
+     */
+    private static $ADD_USER_CONFIG = [];
+    
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        // perform any parent class functions
+        parent::initController($request, $response, $logger);
+
+        self::$ADD_USER_CONFIG['old'] = function ($key) {
+            return $this->request->getPost($key);
+        };
+        // there's no need to load departments when user is not logged in
+        if (!session()->get('user')) return;
+        self::$ADD_USER_CONFIG['departments'] = model(Department::class)->findAll();
+    }
     public function login()
     {
-        return view('auth/login');
+        return view('auth/login', self::$ADD_USER_CONFIG);
     }
 
     public function register()
     {
-        $departments = model(Department::class)->findAll();
-        return view('auth/add-user', [
-            'departments' => $departments
-        ]);
+        return view('auth/add-user', self::$ADD_USER_CONFIG);
     }
 
     public function createSession()
@@ -35,29 +54,37 @@ class Auth extends BaseController
         // stop execution if the validation fails
         if (!$this->validate($sessionRules)) {
             return view('auth/login', [
-                'error' => $this->validator->getErrors()
+                'error' => $this->validator->getErrors(),
+                ...self::$ADD_USER_CONFIG
             ]);
         }
 
+        // find an account with that username
         $accounts = model(Account::class);
         $userAccount = $accounts->where('Username', $employee->username)->first();
+
+        // declare password matches variable, defaulting to false
         $passwordMatches = false;
 
+        // if acount was found, verify if password matches
         if ($userAccount) {
-            $passwordMatches = $employee->password === $userAccount->Password;
+            $passwordMatches = password_verify($employee->password, $userAccount->Password);
         }
 
-        // password has to be hashed before comparing, but disabled for plain passwords to work
-        // if (!password_verify($employee->password, $userAccount->password)) {
+        // if password does not match or no account was found
+        // return an error to the user
         if (!$userAccount || !$passwordMatches) {
             return view('auth/login', [
-                'error' => 'Invalid username or password'
+                'error' => 'Invalid username or password',
+                ...self::$ADD_USER_CONFIG
             ]);
         }
 
         // find the employee department
         $employee = model(ModelsEmployee::class)->where('AccountID', $userAccount->ID)->first();
         $departments = model(Department::class)->find($employee->DepartmentID);
+
+        // save department so that we know which menu to display
         $userAccount->department = $departments->Name;
 
         // save the employee data to the session
@@ -69,15 +96,49 @@ class Auth extends BaseController
 
     public function createAccount()
     {
-        $this->validate([
-            'name' => 'required|min_length[3]|max_length[25]',
-            'surname' => 'required|min_length[3]|max_length[25]',
+        $formDataIsValid = $this->validate([
+            'Name' => 'required|min_length[3]|max_length[25]',
+            'Surname' => 'required|min_length[3]|max_length[25]',
+            'Phone' => 'required|min_length[9]|max_length[12]|numeric',
+            'Username' => 'required|min_length[5]|max_length[25]|alpha|is_unique[accounts.Username]',
+            'Email' => 'required|valid_email',
+            'Password' => 'required|min_length[6]|max_length[25]',
+            'DepartmentID' => 'required|numeric|is_not_unique[departments.ID]',
+            'Gender' => 'required|in_list[Male,Female]',
         ]);
-        if (!$this->validator->run()) {
+
+        // if validation failed, show errors
+        if (!$formDataIsValid) {
             return view('auth/add-user', [
+                // add error to existing variables
                 'error' => $this->validator->getErrors(),
+                ...self::$ADD_USER_CONFIG,
             ]);
         }
+
+        // step 1: create an auth account 
+        // so that the user will be able to log in
+        $accounts = model(Account::class);
+        $newAccount = new EntitiesAccount($this->request->getPost());
+
+        // hash password 
+        $newAccount->Password = password_hash($newAccount->Password, PASSWORD_DEFAULT);
+
+        // save auth account to db
+        $accounts->save($newAccount);
+
+        // step 2: create an employee account
+        // to store the user details
+        $employees = model(ModelsEmployee::class);
+        $newEmployee = new Employee($this->request->getPost());
+
+        // get employees account id, required to link employee 
+        $newEmployee->AccountID = $accounts->getInsertID();
+
+        // save employee to database
+        $employees->save($newEmployee);
+
+        return redirect('/');
     }
 
     public function viewPassword()
@@ -100,8 +161,14 @@ class Auth extends BaseController
         $accounts = model(Account::class);
         $employee = session()->get('user');
         $employee->Password = trim($this->validator->getValidated()['password']);
+
+        // hash password
+        $employee->Password = password_hash($employee->Password, PASSWORD_DEFAULT);
+
+        // save new password
         $accounts->save($employee);
 
+        // go to menu/home page
         return redirect('/');
     }
 
