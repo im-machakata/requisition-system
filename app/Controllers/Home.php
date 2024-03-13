974b85515a22870588fec806f5d79ea06f894eca
Isaac Machakata <isaac@caasi.co.zw>
2024-03-13 09:41:50 +0200

    refactor: route users to their respective views

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,20 @@
 
 namespace App\Controllers;
 
+use App\Entities\Employee;
+
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('home/index');
+        $user = session('user');
+        switch ($user->department) {
+            case "ACCOUNTS":
+                return view('home/accounts');
+            case "SUPERVISOR":
+                return view('home/supervisor');
+            default:
+                return view('home/index');
+        }
     }
 }
