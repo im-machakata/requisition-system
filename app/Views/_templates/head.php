12dce6bab0a095be1664c2de618a3ded318cbfa6
Isaac Machakata <isaac@caasi.co.zw>
2024-03-13 16:00:24 +0200

    refactor: add font awesome icons

--- app/Views/_templates/head.php
+++ app/Views/_templates/head.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/static/css/bootstrap.min.css?version=5.3.3">
+    <link rel="stylesheet" href="/static/css/fas/all.css?version=6.0.0-alpha2">
     <link rel="stylesheet" href="/static/css/style.css?revalidate=<?= time() ?>">
     <link rel="shortcut icon" href="/static/images/core5.png" type="image/png">
     <script src="/static/js/jquery.3.7.1.min.js" defer async></script>
