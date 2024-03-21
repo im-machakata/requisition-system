d2b494ae17a81be0d88ae0b99c432aa55ce2c824
Isaac Machakata <isaac@caasi.co.zw>
2024-03-21 23:35:43 +0200

    refactor: show alerts when messages present

--- app/Views/_templates/alerts.php
+++ app/Views/_templates/alerts.php
@@ -1,7 +1,7 @@
-<?php if ($this->data['error']) :
+<?php if (isset($this->data['error'])) :
     $this->setVar('error', $this->data['error']);
     echo $this->include('_templates/messages/errors');
-elseif ($this->data['success']) :
+elseif (isset($this->data['success'])) :
     $this->setVar('success', $this->data['success']);
     echo $this->include('_templates/messages/success');
 endif;
