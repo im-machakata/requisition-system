bad215402b28beb097332fa96fd0eb4ffb281305
Isaac Machakata <isaac@caasi.co.zw>
2024-03-21 10:49:13 +0200

    chore: add requisition table

---
+++ app/Database/Migrations/2024-03-21-073059_Requisition.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Requisition extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'ID' => [
+                'type' => 'INT',
+                'null' => true,
+                'unique' => true,
+                'constraint' => 8,
+                'auto_increment' => true
+            ],
+            'AccountID' => [
+                'type' => 'INT',
+                'constraint' => 8
+            ],
+            'Amount' => [
+                'type' => 'DECIMAL',
+                'constraint' => '12.2',
+            ],
+            'Reason' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            ],
+            'Type' => [
+                'type' => 'ENUM',
+                'constraint' => [
+                    'Petty_Cash',
+                    'Travel_Subsistence',
+                    'Advanced_Salary',
+                ],
+            ],
+            'Status' => [
+                'type' => 'ENUM',
+                'constraint' => [
+                    'Submitted',
+                    'Supervisor_Pending',
+                    'Supervisor_Approved',
+                    'Finance_Pending',
+                    'Finance_Disbursed',
+                    'Rejected'
+                ],
+                'default' => 'Submitted'
+            ],
+            'CreatedAt' => [
+                'type' => 'INT',
+                'null' => true,
+            ],
+            'UpdatedAt' => [
+                'type' => 'INT',
+                'null' => true,
+            ],
+            'DeletedAt' => [
+                'type' => 'INT',
+                'null' => true,
+            ]
+        ]);
+
+        $this->forge->addPrimaryKey('ID');
+        $this->forge->addForeignKey('AccountID','accounts','ID');
+        $this->forge->createTable('requisitions');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('requisitions');
+    }
+}
