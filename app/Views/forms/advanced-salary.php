9d975477b0a60ef7b32dbbe14209d8d618321b5a
Isaac Machakata <isaac@caasi.co.zw>
2024-03-21 17:33:36 +0200

    refactor: adjust form fields names

--- app/Views/forms/advanced-salary.php
+++ app/Views/forms/advanced-salary.php
@@ -11,14 +11,14 @@ echo $this->include('_templates/head'); ?>
         <form action="/requests/advanced-salaries" method="post" class="col-12 row mt-2">
             <div class="col-lg-12">
                 <div class="mb-4">
-                    <label for="amount" class="form-label">Amount</label>
-                    <input type="number" min="1" name="amount" id="amount" class="form-control" placeholder="50.00" autocomplete="off" required>
+                    <label for="Amount" class="form-label">Amount</label>
+                    <input type="number" min="1" name="Amount" id="Amount" class="form-control" placeholder="50.00" autocomplete="off" required>
                 </div>
             </div>
             <div class="col-lg-12">
                 <div class="mb-4">
-                    <label for="reason" class="form-label">Reason</label>
-                    <textarea type="text" name="reason" id="reason" class="form-control" placeholder="I want to buy a new phone..." required></textarea>
+                    <label for="Reason" class="form-label">Reason</label>
+                    <textarea type="text" name="Reason" id="Reason" class="form-control" placeholder="I want to buy a new phone..." required></textarea>
                 </div>
             </div>
             <div class="mb-2 col-12">
