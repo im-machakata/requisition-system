50a6887d9757e9cc356316c886c757f47f451f78
Isaac Machakata <isaac@caasi.co.zw>
2024-03-21 10:50:09 +0200

    style: adjust subtitle font size

--- app/Views/_templates/header.php
+++ app/Views/_templates/header.php
@@ -3,5 +3,5 @@
         <img src="/static/images/core5.png" alt="System Logo" class="img-flud rounded my-4" height="80">
     </a>
     <h1 class="fw-bold h5">Municipality of Rutenga</h1>
-    <p class="mb-4 text-muted h5">Online Requisition System</p>
+    <p class="text-muted h6">Online Requisition System</p>
 </div>
