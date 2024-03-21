60ad86217cecadc196e9ac0bde92c96c6b8432d9
Isaac Machakata <isaac@caasi.co.zw>
2024-03-21 06:33:14 +0200

    refactor: add author meta tag

--- app/Views/_templates/head.php
+++ app/Views/_templates/head.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en-ZW">
 
 <head>
-    <meta charset="UTF-8">
+    <meta name="author" content="Memory">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/static/css/bootstrap.min.css?version=5.3.3">
     <link rel="stylesheet" href="/static/css/fas/all.css?version=6.0.0-alpha2">
