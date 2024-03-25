f6778bf837ce86878d9b6784e11a54971f462b26
Isaac Machakata <isaac@caasi.co.zw>
2024-03-25 16:40:48 +0200

    refactor: setter functions for entities

--- app/Entities/Requisition.php
+++ app/Entities/Requisition.php
@@ -9,24 +9,25 @@ class Requisition extends Entity
     protected $datamap = [];
     protected $dates   = ['CreatedAt', 'UpdatedAt', 'DeletedAt'];
     protected $casts   = [];
+
     public function setType(string $type)
     {
-        $this->__set('Type', $type);
+        $this->attributes['Type'] = $type;
         return $this;
     }
     public function setAmount(float $amount)
     {
-        $this->__set('Amount', $amount);
+        $this->attributes['Amount'] = $amount;
         return $this;
     }
     public function setAccountID(int $accountID)
     {
-        $this->__set('AccountID', $accountID);
+        $this->attributes['AccountID'] = $accountID;
         return $this;
     }
     public function setStatus(string $status)
     {
-        $this->__set('Status', $status);
+        $this->attributes['Status'] = $status;
         return $this;
     }
 }
