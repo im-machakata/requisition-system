52db6db1d4fcbc36aeffdf858410e8ab0218efbb
Isaac Machakata <isaac@caasi.co.zw>
2024-03-25 14:52:42 +0200

    refactor: calculate date differences

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,5 +1,15 @@
 $(function () {
-    $('.alert').on('click', function () {
-        $(this).hide();
+    $("#OutFrom, #OutTo").on('change', function () {
+        var toDate = $("#OutTo").val();
+        var fromDate = $("#OutFrom").val();
+
+        // Parse the input date string into a Date object
+        var dates = [
+            new Date(fromDate),
+            new Date(toDate),
+        ];
+
+        var diffInDays = Math.floor((dates[1] - dates[0]) / (1000 * 60 * 60 * 24));
+        $("#Days").val(diffInDays);
     });
 });
