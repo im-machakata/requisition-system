61d06d582c55534461ab4c497d39fc7a8ae91c16
Isaac Machakata <isaac@caasi.co.zw>
2024-03-25 11:55:48 +0200

    refactor: remove index.php in pagination urls

--- app/Views/_templates/pagination.php
+++ app/Views/_templates/pagination.php
@@ -4,12 +4,12 @@
     <ul class="pagination ">
         <?php if ($pager->hasPrevious()) : ?>
             <li class="page-item <?= $pager->hasPrevious() ? '' : 'disabled' ?>">
-                <a class="page-link border" href="<?= $pager->getFirst() ?>" aria-label="<?= lang('Pager.first') ?>">
+                <a class="page-link border" href="<?= str_replace('/index.php','',$pager->getFirst()) ?>" aria-label="<?= lang('Pager.first') ?>">
                     <span aria-hidden="true"><?= lang('Pager.first') ?></span>
                 </a>
             </li>
             <li class="page-item">
-                <a class="page-link border" href="<?= $pager->getPrevious() ?>" aria-label="<?= lang('Pager.previous') ?>">
+                <a class="page-link border" href="<?= str_replace('/index.php','',$pager->getPrevious()) ?>" aria-label="<?= lang('Pager.previous') ?>">
                     <span aria-hidden="true"><?= lang('Pager.previous') ?></span>
                 </a>
             </li>
@@ -17,7 +17,7 @@
 
         <?php foreach ($pager->links() as $link) : ?>
             <li class="page-item<?= $link['active'] ? ' active' : '' ?>">
-                <a class="page-link border" href="<?= $link['uri'] ?>">
+                <a class="page-link border" href="<?= str_replace('/index.php','',$link['uri']) ?>">
                     <?= $link['title'] ?>
                 </a>
             </li>
@@ -25,12 +25,12 @@
 
         <?php if ($pager->hasNext()) : ?>
             <li class="page-item">
-                <a class="page-link border" href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>">
+                <a class="page-link border" href="<?= str_replace('/index.php','',$pager->getNext()) ?>" aria-label="<?= lang('Pager.next') ?>">
                     <span aria-hidden="true"><?= lang('Pager.next') ?></span>
                 </a>
             </li>
             <li class="page-item">
-                <a class="page-link border" href="<?= $pager->getLast() ?>" aria-label="<?= lang('Pager.last') ?>">
+                <a class="page-link border" href="<?= str_replace('/index.php','',$pager->getLast()) ?>" aria-label="<?= lang('Pager.last') ?>">
                     <span aria-hidden="true"><?= lang('Pager.last') ?></span>
                 </a>
             </li>
