0053517ae4cf19ee1e9c2c46f9eae786932313ce
Isaac Machakata <isaac@caasi.co.zw>
2024-03-25 16:41:41 +0200

    refactor: setup home controller for different users

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,20 +2,16 @@
 
 namespace App\Controllers;
 
-use App\Entities\Employee;
-
 class Home extends BaseController
 {
     public function index(): string
     {
-        $user = session('user');
-        switch ($user->department) {
-            case "ACCOUNTS":
-                return view('home/accounts');
-            case "SUPERVISOR":
-                return view('home/supervisor');
-            default:
-                return view('home/index');
+        $user = $this->session->get('user');
+        $department = strtolower($user->department);
+        try {
+            return view('home/' . $department);
+        } catch (\Exception $e) {
+            return view('home/index');
         }
     }
 }
