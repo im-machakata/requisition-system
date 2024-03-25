dd856f95c9c0ec8eb80a0e01a2809cb9f58a4d45
Isaac Machakata <isaac@caasi.co.zw>
2024-03-25 10:03:17 +0200

    refcator: record new petty cash

--- app/Controllers/Requisition.php
+++ app/Controllers/Requisition.php
@@ -3,7 +3,9 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Entities\Requisition as EntitiesRequisition;
 use App\Models\Requisition as ModelsRequisition;
+use CodeIgniter\Config\Services;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Requisition extends BaseController
@@ -14,10 +16,38 @@ class Requisition extends BaseController
         $requisitions = model(ModelsRequisition::class);
         return view('forms/petty-cash', [
             ...self::$ADD_USER_CONFIG,
-            'requisitions' => $requisitions
-                ->where('Type', 'Petty_Cash')
-                ->where('AccountID', $this->session->get('user')->ID)
-                ->findAll()
+            'requisitions' => $requisitions->getPettyCash($this->account->ID),
+            'pager' => $requisitions->pager
+        ]);
+    }
+    public function recordPettyCash()
+    {
+        $requisitions = model(ModelsRequisition::class);
+        $formIsValid = $this->validate([
+            'Amount' => 'required|numeric|min_length[1]|max_length[15]',
+            'Reason' => 'required|min_length[15]|max_length[255]'
+        ]);
+
+        if (!$formIsValid) {
+            return view('forms/petty-cash', [
+                ...self::$ADD_USER_CONFIG,
+                'error' => $this->validator->getErrors(),
+                'requisitions' => $requisitions->getPettyCash($this->account->ID),
+                'pager' => $requisitions->pager
+            ]);
+        }
+
+        // fill in and save new petty cash requisition
+        $requisition = new EntitiesRequisition($this->validator->getValidated());
+        $requisition->AccountID = $this->account->ID;
+        $requisition->Type = $requisitions::PETTY_CASH;
+        $recorded = $requisitions->save($requisition);
+
+        return view('forms/petty-cash', [
+            ...self::$ADD_USER_CONFIG,
+            'success' => 'Petty cash has been recorded.',
+            'requisitions' => $requisitions->getPettyCash($this->account->ID),
+            'pager' => $requisitions->pager
         ]);
     }
 
