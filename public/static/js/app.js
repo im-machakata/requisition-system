1636d35b593c52d21f0e7d0a490e1ee91cde510c
Isaac Machakata <isaac@caasi.co.zw>
2024-03-27 03:57:22 +0200

    refactor: disable and enable submit button if date

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -2,9 +2,15 @@ $(function () {
     $("#OutFrom, #OutTo").on('change', function () {
         var toDate = $("#OutTo").val();
         var fromDate = $("#OutFrom").val();
+        const toggleSubmitButton = (days) => {
+            $('#Submit').attr('disabled', (days == 0));
+        }
 
         // do not attempt to calculate date differences when one of the dates if missing
-        if (!toDate || !fromDate) return;
+        if (!toDate || !fromDate) {
+            toggleSubmitButton(0);
+            return
+        };
 
         // Parse the input date string into a Date object
         var dates = [
@@ -13,6 +19,7 @@ $(function () {
         ];
 
         var diffInDays = Math.floor((dates[1] - dates[0]) / (1000 * 60 * 60 * 24));
+        toggleSubmitButton(diffInDays);
         $("#Days").val(diffInDays);
     });
 });
