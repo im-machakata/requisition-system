675519457e81d264bdca645921929ca8ac0e3e2b
Isaac Machakata <isaac@caasi.co.zw>
2024-04-07 22:46:58 +0200

    style: add more space before menu refactor: change check requisitions menu

--- app/Views/home/accounts.php
+++ app/Views/home/accounts.php
@@ -6,66 +6,64 @@ echo $this->include('_templates/head'); ?>
         <?= $this->include('_templates/header'); ?>
         <?= $this->include('_templates/user-type'); ?>
         <?= $this->include('_templates/alerts'); ?>
-        <div class="col-12 mb-4">
-            <div class="col-12 row g-4 mb-4 menu">
-                <div class="col-sm-6 col-lg-3">
-                    <div class="card shadow-none border-primary border-2">
-                        <div class="card-body">
-                            <a href="advanced-salaries" class="text-decoration-none text-center">
-                                <i class="fa-thin fa-money-check-dollar-pen fa-6x my-0 d-block"></i>
-                                <h3 class="card-title h5 mb-3">Advanced Salary</h3>
-                            </a>
-                        </div>
+        <div class="col-12 row g-4 mb-4 menu">
+            <div class="col-sm-6 col-lg-3">
+                <div class="card shadow-none border-primary border-2">
+                    <div class="card-body">
+                        <a href="advanced-salaries" class="text-decoration-none text-center">
+                            <i class="fa-thin fa-money-check-dollar-pen fa-6x my-0 d-block"></i>
+                            <h3 class="card-title h5 mb-3">Advanced Salary</h3>
+                        </a>
                     </div>
                 </div>
-                <div class="col-sm-6 col-lg-3">
-                    <div class="card shadow-none border-primary border-2">
-                        <div class="card-body">
-                            <a href="travel-and-subsistencies" class="text-decoration-none text-center">
-                                <i class="fa-thin fa-plane-departure fa-6x my-0 d-block"></i>
-                                <h3 class="card-title h5 mb-3">Travel &amp; Subsistences</h3>
-                            </a>
-                        </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="card shadow-none border-primary border-2">
+                    <div class="card-body">
+                        <a href="travel-and-subsistencies" class="text-decoration-none text-center">
+                            <i class="fa-thin fa-plane-departure fa-6x my-0 d-block"></i>
+                            <h3 class="card-title h5 mb-3">Travel &amp; Subsistences</h3>
+                        </a>
                     </div>
                 </div>
-                <div class="col-sm-6 col-lg-3">
-                    <div class="card shadow-none border-primary border-2">
-                        <div class="card-body">
-                            <a href="petty-cash" class="text-decoration-none text-center">
-                                <i class="fa-thin fa-money-bill-1 fa-6x my-0 d-block"></i>
-                                <h3 class="card-title h5 mb-3">Petty Cash</h3>
-                            </a>
-                        </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="card shadow-none border-primary border-2">
+                    <div class="card-body">
+                        <a href="petty-cash" class="text-decoration-none text-center">
+                            <i class="fa-thin fa-money-bill-1 fa-6x my-0 d-block"></i>
+                            <h3 class="card-title h5 mb-3">Petty Cash</h3>
+                        </a>
                     </div>
                 </div>
-                <div class="col-sm-6 col-lg-3">
-                    <div class="card shadow-none border-primary border-2">
-                        <div class="card-body">
-                            <a href="authorize-requisitions" class="text-decoration-none text-center">
-                                <i class="fa-thin fa-signature fa-6x my-0 d-block"></i>
-                                <h3 class="card-title h5 mb-3">Authorization</h3>
-                            </a>
-                        </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="card shadow-none border-primary border-2">
+                    <div class="card-body">
+                        <a href="authorize-requisitions" class="text-decoration-none text-center">
+                            <i class="fa-thin fa-clipboard-list-check fa-6x my-0 d-block"></i>
+                            <h3 class="card-title h5 mb-3">Check Requisitions</h3>
+                        </a>
                     </div>
                 </div>
-                <div class="col-sm-6 col-lg-3">
-                    <div class="card shadow-none border-primary border-2">
-                        <div class="card-body">
-                            <a href="/auth/change-password" class="text-decoration-none text-center">
-                                <i class="fa-thin fa-unlock-keyhole fa-6x my-0 d-block"></i>
-                                <h3 class="card-title h5 mb-3">Change Password</h3>
-                            </a>
-                        </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="card shadow-none border-primary border-2">
+                    <div class="card-body">
+                        <a href="/auth/change-password" class="text-decoration-none text-center">
+                            <i class="fa-thin fa-unlock-keyhole fa-6x my-0 d-block"></i>
+                            <h3 class="card-title h5 mb-3">Change Password</h3>
+                        </a>
                     </div>
                 </div>
-                <div class="col-sm-6 col-lg-3">
-                    <div class="card shadow-none border-primary border-2">
-                        <div class="card-body">
-                            <a href="/auth/logout" class="text-decoration-none text-center">
-                                <i class="fa-thin fa-arrow-right-from-bracket fa-6x my-0 d-block"></i>
-                                <h3 class="card-title h5 mb-3">Logout</h3>
-                            </a>
-                        </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="card shadow-none border-primary border-2">
+                    <div class="card-body">
+                        <a href="/auth/logout" class="text-decoration-none text-center">
+                            <i class="fa-thin fa-arrow-right-from-bracket fa-6x my-0 d-block"></i>
+                            <h3 class="card-title h5 mb-3">Logout</h3>
+                        </a>
                     </div>
                 </div>
             </div>
