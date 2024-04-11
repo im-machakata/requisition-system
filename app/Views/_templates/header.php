61e0420dcb5b461da1ed99df2e358d212b51aecc
Isaac Machakata <isaac@caasi.co.zw>
2024-04-11 15:29:50 +0200

    refactor: change council name

--- app/Views/_templates/header.php
+++ app/Views/_templates/header.php
@@ -2,6 +2,6 @@
     <a href="/">
         <img src="/static/images/core5.png" alt="System Logo" class="img-flud rounded my-4" height="80">
     </a>
-    <h1 class="fw-bold h5">Municipality of Rutenga</h1>
+    <h1 class="fw-bold h5">Municipality of Beitbridge</h1>
     <p class="text-muted h6">Online Requisition System</p>
 </div>
